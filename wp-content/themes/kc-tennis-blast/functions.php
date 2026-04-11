f22bb4217d299942f832115ee9807093b6224285
Scott Dyer <scott.dyer@my.jcu.edu.au>
2026-04-11 21:55:18 +1000

    Add load_custom_fonts function

--- wp-content/themes/kc-tennis-blast/functions.php
+++ wp-content/themes/kc-tennis-blast/functions.php
@@ -18,3 +18,14 @@ function kc_tennis_blast_setup() {
 	add_editor_style( 'assets/css/editor.css' );
 }
 add_action( 'after_setup_theme', 'kc_tennis_blast_setup' );
+
+function kc_tennis_blast_load_custom_fonts() {
+	wp_enqueue_style(
+		'kc-tennis-blast-fonts',
+		'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Alumni+Sans:wght@400;600;700&display=swap',
+		array(),
+		null
+	);
+}
+add_action( 'wp_enqueue_scripts', 'kc_tennis_blast_load_custom_fonts' );
+add_action( 'enqueue_block_assets', 'kc_tennis_blast_load_custom_fonts' );
